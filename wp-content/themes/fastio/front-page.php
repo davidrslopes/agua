4f401f6ba1685ffe07fb5f63824a8ba64d7e11c4
David RS Lopes <davidrslopes@gmail.com>
2017-12-14 16:58:01 +0000

    Added Frontpage file

---
+++ wp-content/themes/fastio/front-page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The front page file.
+ *
+ */
+
+get_header();
+?>
+<div class="wrapper" id="wrapper-home">
+
+	<div class="col-xs-12" id="content" tabindex="-1">
+
+		<div class="row">
+			<!-- ******************* The Video Area ******************* -->
+			
+			<!-- TODO HUGO -->
+		</div><!-- .row -->
+
+	</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
