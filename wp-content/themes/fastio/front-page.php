613c8ff6c62cc550bf5e3875dac49847da588d34
David RS Lopes <davidrslopes@gmail.com>
2017-12-18 23:25:32 +0000

    Full Responsive Background Video

--- wp-content/themes/fastio/front-page.php
+++ wp-content/themes/fastio/front-page.php
@@ -7,27 +7,22 @@
 get_header();
 
 $fastio_home_video = get_theme_mod( 'fastio_home_video' );
+$fastio_home_image_fallback = get_theme_mod( 'fastio_home_image_fallback' );
 ?>
-<div class="wrapper" id="wrapper-home">
+<!-- ******************* The Video Area ******************* -->
+<video class="fastio-video" poster="<?php echo $fastio_home_image_fallback; ?>" autoplay loop muted style="background: url(<?php echo $fastio_home_image_fallback; ?>) no-repeat;">
+	<source src="<?php echo $fastio_home_video; ?>" type="video/mp4">
+</video>
+<!-- ******************* The Video Area End ******************* -->
 
-	<div class="col-xs-12" id="content" tabindex="-1">
-
-		<div class="row">
-			<!-- ******************* The Video Area ******************* -->
-			
-			<video width="100%" width="100%" style="width:100vw;height:100vh">
-				<source src="<?php echo $fastio_home_video; ?>" type="video/mp4">
-				Your browser does not support the video tag.
-			</video>
-			
-			<pre>
-				<?php print_r($fastio_home_video); ?>
-			</pre>
-			<!-- TODO HUGO -->
-		</div><!-- .row -->
-
+<!-- ******************* Content ******************* -->
+<div class="wrapper wrapper-home" id="wrapper-home">
+	<div class="container">
+		<div class="col-xs-12" id="content" tabindex="-1">
+			<!-- Content goes here -->
+		</div>
 	</div><!-- Container end -->
-
 </div><!-- Wrapper end -->
+<!-- ******************* Content End ******************* -->
 
 <?php get_footer(); ?>
