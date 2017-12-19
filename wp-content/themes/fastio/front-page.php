1359c64ee0a18e552ea1d17da299714f06383837
HCarvalho <hugocarvalho@sapo.pt>
2017-12-19 00:57:59 +0000

    Add plays inline tag

--- wp-content/themes/fastio/front-page.php
+++ wp-content/themes/fastio/front-page.php
@@ -11,7 +11,7 @@ $fastio_home_image_fallback = get_theme_mod( 'fastio_home_image_fallback' ); //T
 ?>
 <!-- ******************* The Video Area ******************* -->
 <?php /* Nota IMPORTANTE: Temos de verificar como deve ser o comportamento mobile desta area, apenas permitir a visualização do vídeo em WIFI era o ideal, em 3G este video não deve ser incluído, e deverá comprimir-se em vários formatos para que seja possivel a responsividade também em relação ao peso em mb do site. */ ?>
-<video class="fastio-video" poster="<?php echo $fastio_home_image_fallback; ?>" autoplay loop muted style="background: url(<?php echo $fastio_home_image_fallback; ?>) no-repeat;">
+<video class="fastio-video" poster="<?php echo $fastio_home_image_fallback; ?>" autoplay loop muted plays-inline style="background: url(<?php echo $fastio_home_image_fallback; ?>) no-repeat;">
 	<source src="<?php echo $fastio_home_video; ?>" type="video/mp4">
 </video>
 <!-- ******************* The Video Area End ******************* -->
