aeb906dfe7b3c6d08a53ea74bd623a5e2f0a4127
HCarvalho <hugocarvalho@sapo.pt>
2018-01-02 16:21:58 +0000

    Page Historia work with custom post type

--- wp-content/themes/fastio/page-templates/historiafastio.php
+++ wp-content/themes/fastio/page-templates/historiafastio.php
@@ -20,57 +20,53 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="col-md-12 content-area" id="primary">
 
 				<main class="site-main" id="main" role="main">
-
-					<?php while ( have_posts() ) : the_post(); ?>
+                                    
+                        <?php $args = array('post_type' => 'historia'); ?>
+                        <?php $loop = new WP_Query($args); ?>
+                        <?php if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
 						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
-						<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-
-							comments_template();
-
-						endif;
-						?>
-
 					<?php endwhile; // end of the loop. ?>
-
+                        <?php else: ?>
+                            <h1>No posts here!</h1>
+                        <?php endif; ?>
+                        <?php wp_reset_postdata(); ?>
 				</main><!-- #main -->
 
 			</div><!-- #primary -->
 
 		</div><!-- .row end -->
-                
-                <div class="row">
-                    <div class="col-xs-3">
+
+	</div><!-- Container end -->
+        
+        <div class="row">
+                    <div class="col-md-3">
                         <?php $image = wp_get_attachment_image_src(get_field('imagem_1'), 'full'); ?>
                         <a href="<?php the_field('link_imagem_1'); ?>" class="thumbnail">
                              <?php $image = wp_get_attachment_image_src(get_field('imagem_1'), 'full'); ?>
                              <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('imagem_1')) ?>" class="img-responsive">
                         </a>
                     </div>
-                    <div class="col-xs-3">
+                    <div class="col-md-3">
                        <a href="<?php the_field('link_imagem_2'); ?>" class="thumbnail">
                             <?php $image2 = wp_get_attachment_image_src(get_field('imagem_2'), 'full'); ?>
                            <img src="<?php echo $image2[0]; ?>" alt="<?php echo get_the_title(get_field('imagem_2')) ?>" class="img-responsive">
                        </a>
                    </div>
-                    <div class="col-xs-3">
+                    <div class="col-md-3">
                         <a href="<?php the_field('link_imagem_3'); ?>" class="thumbnail">
                             <?php $image3 = wp_get_attachment_image_src(get_field('imagem_3'), 'full'); ?>
                                <img src="<?php echo $image3[0]; ?>" alt="<?php echo get_the_title(get_field('imagem_3')) ?>" class="img-responsive">
                         </a>
                     </div>
-                    <div class="col-xs-3">
+                    <div class="col-md-3">
                         <a href="<?php the_field('link_imagem_4'); ?>" class="thumbnail">
                                 <?php $image4 = wp_get_attachment_image_src(get_field('imagem_4'), 'full'); ?>
                                <img src="<?php echo $image4[0]; ?>" alt="<?php echo get_the_title(get_field('imagem_4')) ?>" class="img-responsive">
                         </a>
                     </div>
-                </div>
-
-	</div><!-- Container end -->
+        </div>
 
 </div><!-- Wrapper end -->
 
