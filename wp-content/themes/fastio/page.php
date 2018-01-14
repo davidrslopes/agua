612a6de6c95493460c17ecc08816a970de601903
HCarvalho <hugocarvalho@sapo.pt>
2018-01-14 23:42:56 +0000

    Cookies Page , Aviso Legal and Contactos

--- wp-content/themes/fastio/page.php
+++ wp-content/themes/fastio/page.php
@@ -16,46 +16,97 @@ $container   = get_theme_mod( 'understrap_container_type' );
 $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 
 ?>
+<?php 
+//Section 1 : Hero 1
+$hero_1 = get_field('commonpage-section-1');
+if(!empty($hero_1)):
+?>
 
-<div class="wrapper" id="page-wrapper">
-
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
-
+<!-- ******************* The Hero Section ******************* -->
+<section class="fastio-hero" style="background-image:url('<?php echo $hero_1['bg-img']; ?>');">
+	<?php if(!empty($hero_1['title'])): ?>
+	<div class="container-fluid">
 		<div class="row">
-
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
-
-			<main class="site-main" id="main">
-
-				<?php while ( have_posts() ) : the_post(); ?>
+			<header class="fastio-hero-header col-12">
+				<h1 class="text-center"><?php echo $hero_1['title'];?></h1>
+			</header>
+		</div>
+	</div>
+	<?php endif; ?>
+</section><!-- .fastio-hero -->
+<?php endif; ?>
+<?php 
+//Section 2 : Imagem Lateral
+$side_image = get_field('seccao_2:_imagem_lateral');
+?>
+<section class="fastio-common-pages">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-3">
+				<div class="list-group" id="list-tab" role="tablist">
+					<a class="list-group-item list-group-item-action active" id="nav-tab-politica" data-toggle="list" href="#tab-politica" role="tab" aria-controls="tab-politica"><?php _e( 'Política Cookies', 'understrap' ); ?></a>
+					<a class="list-group-item list-group-item-action" id="nav-tab-avisolegal" data-toggle="list" href="#tab-avisolegal" role="tab" aria-controls="tab-avisolegal"><?php _e( 'Aviso Legal', 'understrap' ); ?></a>
+					<a class="list-group-item list-group-item-action" id="nav-tab-contato" data-toggle="list" href="#tab-contactos" role="tab" aria-controls="tab-contactos"><?php _e( 'Contato', 'understrap' ); ?></a>
+				</div>
+			</div>
+			<div class="col-6">
+				<div class="tab-content">
+					<div class="tab-pane fade show active" id="tab-politica" role="tabpanel" aria-labelledby="tab-politica">
+                                        <main class="site-main" id="main">          
+				<?php 
+                                $my_query = new WP_Query('page_id=185');
+                                while ($my_query->have_posts()) : $my_query->the_post();
+                                $do_not_duplicate = $post->ID;?>
+                                <?php if(is_page(icl_object_id(185, 'page'))) : ?>
 
 					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
-
+                                <?php endif;?>
 				<?php endwhile; // end of the loop. ?>
 
-			</main><!-- #main -->
+                                        </main><!-- #main -->
+                                    </div>
+					<div class="tab-pane fade" id="tab-avisolegal" role="tabpanel" aria-labelledby="tab-avisolegal">
+                                                
+                                        <main class="site-main" id="main">
 
-		</div><!-- #primary -->
+				<?php 
+                                $my_query2 = new WP_Query('page_id=188');
+                                while ($my_query2->have_posts()) : $my_query2->the_post();
+                                $do_not_duplicate = $post->ID;?>
+                                <?php if(is_page(icl_object_id(188, 'page'))) : ?>
 
-		<!-- Do the right sidebar check -->
-		<?php if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : ?>
+					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
-			<?php get_sidebar( 'right' ); ?>
+                                <?php endif;?>
+				<?php endwhile; // end of the loop. ?>
 
-		<?php endif; ?>
+                                        </main><!-- #main -->
+					</div>
+					<div class="tab-pane fade" id="tab-contato" role="tabpanel" aria-labelledby="tab-contactos">
+					
+                                        <main class="site-main" id="main">
 
-	</div><!-- .row -->
+				<?php 
+                                $my_query3 = new WP_Query('page_id=191');
+                                while ($my_query3->have_posts()) : $my_query3->the_post();
+                                $do_not_duplicate = $post->ID;?>
+                                <?php if(is_page(icl_object_id(191, 'page'))) : ?>
 
-</div><!-- Container end -->
+					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
-</div><!-- Wrapper end -->
+                                <?php endif;?>
+				<?php endwhile; // end of the loop. ?>
 
+                                        </main><!-- #main -->	
+                                           
+					</div>
+				</div>
+			</div>
+                    <div class="col-3">
+                        <img src="<?php echo $side_image['bg-img']; ?>" class="img-responsive"/>
+                    </div>
+		</div>
+	</div>
+</section><!-- .fastio-commonpages -->
 <?php get_footer(); ?>
