ac12216a08db28585a90fee3814e4bca5ef7e561
David RS Lopes <davidrslopes@gmail.com>
2018-01-18 19:18:58 +0000

    EO: Locais e Mapa Inicial

--- wp-content/themes/fastio/js/fastio.js
+++ wp-content/themes/fastio/js/fastio.js
@@ -250,7 +250,27 @@ jQuery( function($) {
 			if(debug){console.log('This image caption id is: '+caption_id);}
 		});
 	}
-	
+	//SLIDER
+	$('#fastio-places-slide').on('slide.bs.carousel', function (e) {
+		
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
 	//Log Copy Check
 	console.log('%c©2018 SAVAGE Agency | DavidRSLopes & Hugo Carvalho', 'background: #39ee7c; background: -moz-linear-gradient(left, #39ee7c 0%, #252525 100%); background: -webkit-linear-gradient(left, #39ee7c 0%,#252525 100%); background: linear-gradient(to right, #39ee7c 0%,#252525 100%); color: #241f20; line-height:60px; font-family: Allerta,sans-serif; font-size:12px; padding: 10px 25px 10px 10px; height:107px; letter-spacing:2px;' );
 });
--- wp-content/themes/fastio/page-templates/explore-o-geres.php
+++ wp-content/themes/fastio/page-templates/explore-o-geres.php
@@ -27,6 +27,71 @@ if(!empty($hero_1)):
 </section><!-- .fastio-hero -->
 <?php endif; ?>
 
+<?php 
+$args = array(
+	child_of => get_the_ID()
+);
+$subpages = get_pages($args);
+if( !empty( $subpages ) ): ?>
+<!-- ******************* The Places Display Section ******************* -->
+<section class="fastio-places">
+	<div id="fastio-places-slide" class="carousel slide" data-ride="carousel" data-interval="9000">
+		<div class="carousel-inner row w-100 mx-auto" role="listbox">
+<?php
+	$total = count($subpages);
+	$i = 0;
+	foreach( $subpages as $page ):
+	$i++;
+?>
+			<article class="carousel-item col-md-3<?php if($i===1){ echo' active'; }?>">
+				<a href="<?php echo get_page_link( $page->ID ); ?>">
+					<img class="img-fluid mx-auto d-block" src="//placehold.it/400x300/000/fff?text=<?php echo $page->post_title; ?>" alt="<?php echo $page->post_title; ?>">
+				</a>
+				<h4>
+					<a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a>
+				</h4>
+			</article>
+
+<?php
+	endforeach;
+?>
+		</div>
+		<a class="carousel-control-prev" href="#fastio-places-slide" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Anterior</span>
+		</a>
+		<a class="carousel-control-next" href="#fastio-places-slide" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Próximo</span>
+		</a>
+	</div>
+</section>
+<?php endif; ?>
+
+<?php 
+$args = array(
+	child_of => get_the_ID()
+);
+$subpages = get_pages($args);
+if( !empty( $subpages ) ): ?>
+<!-- ******************* The Map Section ******************* -->
+<section class="fastio-map">
+	<div id="map"></div>
+</section>
+<script>
+	var map;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 11,
+			center: new google.maps.LatLng(41.838648, -8.241580),
+			mapTypeId: 'terrain'
+		});
+	}
+</script>
+<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAg5djlKiQzO9o1bb9fBKT51Crt_LKjPqE&callback=initMap">
+    </script>
+<?php endif; ?>
 
 
 <?php get_footer(); ?>
