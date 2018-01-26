c454d05100d741c7acb2e2b533579e31114112a7
HCarvalho <hugocarvalho@sapo.pt>
2018-01-26 00:05:22 +0000

    Fastio Barbie and Fastio Barbie Subpages ACF Fields and Page Template

---
+++ wp-content/themes/fastio/page-templates/barbie-subpages.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * Template Name: Fastio Barbie Subpaginas
+ *
+ * Template for displaying a specific page
+ *
+ * @package understrap
+ */
+
+get_header();
+?>
+
+
+<?php 
+//Section 1 : Hero 1
+$hero_1 = get_field('barbiesubpages-section-1');
+if(!empty($hero_1)):
+/* Nota IMPORTANTE: Temos de verificar como deve ser o comportamento mobile desta area, apenas permitir a visualização do vídeo em WIFI era o ideal, em 3G este video não deve ser incluído, e deverá comprimir-se em vários formatos para que seja possivel a responsividade também em relação ao peso em mb do site. */
+?>
+<!-- ******************* The Logo and Images Hero Section ******************* -->
+<section class="fastio-images-hero fastio-hotwheels-subpages-1">
+	
+        <div class="container-fluid">
+           <div class="row">    
+            <div class="col-6">
+                <div data-aos="fade-up" data-aos-duration="3000">
+                    <img src="<?php echo $hero_1['img_titulo']; ?>" class="img-fluid float-right"/>
+                </div>
+                <div data-aos="fade-up" data-aos-duration="3000">                
+                <div class="title">
+                    <?php echo $hero_1['texto']; ?>
+                </div></div>
+            </div>
+            <div class="col-6">           
+                <div data-aos="fade-up" data-aos-duration="3000">
+                 <img src="<?php echo $hero_1['img2']; ?>" class="img-fluid bottleimg"/>
+                </div>
+                <div data-aos="fade-up" data-aos-duration="3000">
+                <img src="<?php echo $hero_1['img_hashtag']; ?>" class="img-fluid hashtagimg float-right"/>
+                </div>
+            </div>
+           </div>
+            <div class="row">
+                <div class="col-6">
+                    <div class="icon faa-float animated">
+                        <i class="fa fa-angle-down"></i></div>
+                </div>
+                <div class="col-6">
+                    <div data-aos="fade-up" data-aos-duration="3000">
+                    <img src="<?php echo $hero_1['img1']; ?>" class="img-fluid float-right passadeiraimg"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section><!-- .fastio-images-hero -->
+<?php endif; ?>
+<!-- ******************* The Section2 ******************* -->
+<?php 
+	$section2 = get_field('barbiesubpages-section-2');
+	if(!empty($section2)):
+?>
+<section class="fastio-hotwheels-subpages-section2">
+	<div class="container">
+		<div class="row">
+                    <div class="col-12">
+                        <div data-aos="fade-up" data-aos-duration="3000">
+                            <img src="<?php echo $section2['img1']; ?>" class="img-fluid imgcarrohotwheels"/>
+                            <img src="<?php echo $section2['img2']; ?>" class="img-fluid metaimg"/>
+                        </div>
+                    </div>
+		</div>
+            <div class="row">
+                <div class="col-6"></div>
+                <div class="col-6">
+                    <div data-aos="fade-up" data-aos-duration="3000">
+                        <img src="<?php echo $section2['img_titulo']; ?>" class="img-fluid float-right"/>
+                        <div class="title">
+                            <?php echo $section2['subtitulo']; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+	</div>
+</section><!-- .fastio-hotwheels-section2 -->
+
+<?php endif;?>
+
+<?php 
+//Section 3 : Hero 3
+$section_3 = get_field('barbiesubpages-section-3');
+if(!empty($section_3)):
+?>
+<!-- ******************* Section 3 ******************* -->
+<section class="fastio-hotwheels-subpages-section3">
+        <div class="container-fluid">
+           <div class="row">
+            <div class="col-3"></div>
+            <div class="col-9">
+                <div data-aos="fade-up"data-aos-duration="3000">
+                    <img src="<?php echo $section_3['img1']; ?>" class="img-fluid float-left"/>
+                </div>
+            </div>
+           </div>
+            <div class="row">
+                <div class="col-6">
+                    <div data-aos="fade-up" data-aos-duration="3000">
+                        <img src="<?php echo $section_3['img2']; ?>" class="img-fluid float-left imgbancada"/>
+                    </div>
+                </div>
+                <div class="col-6 titulo-subtitulo">
+                    <div data-aos="fade-up" data-aos-duration="3000">
+                        <img src="<?php echo $section_3['img_titulo']; ?>" class="img-fluid float-left"/>
+                        <div class="title">
+                            <?php echo $section_3['subtitulo']; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section><!-- .fastio-images-section3 -->
+<?php endif; ?>
+<?php
+
+//Section 4 : Slider Subpages
+	if( have_rows('barbiesubpages-section-4-1') ): 
+		$links_count = count(get_field('barbiesubpages-section-4-1'));
+		$links_col = ceil(12 / $links_count);
+ 		if($links_col<6) $links_col = 6;
+?>
+<!-- ******************* The Fastio Links Section ******************* -->
+<section class="fastio-hotwheels-subpages-section4">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- //Section 4 : Subpages -->
+                <?php
+                $hero_4 = get_field('barbiesubpages-section-4');
+                if(!empty($hero_4)):
+                ?>
+                <div class="col-3"></div>
+                <div class="col-9">
+                    <div data-aos="fade-up" data-aos-duration="3000">
+                        <img src="<?php echo $hero_4['img_titulo']; ?>" class="img-fluid float-right escolhecarroimg"/>
+                    </div>
+                </div>
+          
+                <?php endif; ?>
+            </div>
+	<div class="row">
+            <!--Section 4-1 : Subpages Slider -->
+       <div id="subpagescarousel" class="carousel slide" data-ride="carousel" data-interval="9000">
+        <div class="carousel-inner row w-80 mx-auto" role="listbox" >
+        <?php $i = 0; ?>
+        <?php while( have_rows('barbiesubpages-section-4-1') ) : the_row(); 
+        $link_id = get_sub_field('link', false, false);
+        $i++;
+        ?>
+            <div class="carousel-item col-md-3 <?php echo $i==0 ? 'active' : '';?>">
+                <a href="<?php echo get_the_permalink($link_id); ?>">
+		<img src="<?php the_sub_field('img') ?>" alt="" />
+                </a>
+            </div>
+        <?php endwhile; ?>
+    </div>
+           <a class="carousel-control-prev" href="#subpagescarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#subpagescarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+    </div>
+	</div>
+        </div>
+</section><!-- .fastio-hotwheels-subpages-section4 -->
+<?php endif; ?>
+
+<?php get_footer();?>
---
+++ wp-content/themes/fastio/page-templates/barbie.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Template Name: Barbie
+ *
+ * Template for displaying a specific page
+ *
+ * @package understrap
+ */
+
+get_header();
+?>
+
+
+<?php 
+//Section 1 : Hero 1
+$hero_1 = get_field('barbie-section-1');
+if(!empty($hero_1)):
+?>
+<!-- ******************* The Logo and Images Hero Section ******************* -->
+<section class="fastio-images-hero">
+	
+	<?php if(!empty($hero_1['bg-img']) && !empty($hero_1['logo-img'])): ?>
+        <div class="container">
+           <div class="row">
+            <div class="col-6" style="padding-right: 0px;">
+                <img src="<?php echo $hero_1['logo-img']; ?>" class="img-fluid float-right"/>
+                <button class="btn btn-link btn-block fastio-btn-scroll-down"><i class="fa fa-angle-down"></i></button>
+            </div>
+            <div class="col-6" style="padding-right: 0px;">
+                <img src="<?php echo $hero_1['bg-img']; ?>" class="img-fluid "/>
+            </div>
+           </div>
+        </div>
+	<?php endif; ?>
+	
+</section><!-- .fastio-images-hero -->
+<?php endif; ?>
+
+
+
+<!-- ******************* The Titles Feature Section ******************* -->
+<?php 
+	$titles = get_field('barbie-section-2');
+	if(!empty($titles)):
+?>
+<section class="fastio-barbie-titles">
+	<div class="container">
+		<div class="row">
+			<header class="fastio-hero-header col-12">
+                        <?php if(!empty($titles['titulo_1'])): ?><h1 class="title"><?php echo $titles['titulo_1'];?></h1><?php endif; ?> 
+			<?php if(!empty($titles['subtitulo'])): ?><h3 class="subtitle"><?php echo $titles['subtitulo'];?></h3><?php endif; ?> 
+                        </header>
+		</div>
+	</div>
+</section><!-- .fastio-hotwheels-titles -->
+
+<?php endif;?>
+
+
+<?php
+//Section 3 : Subpages
+	if( have_rows('barbie-section-3') ): 
+		$links_count = count(get_field('barbie-section-3'));
+		$links_col = ceil(12 / $links_count);
+ 		if($links_col<6) $links_col = 4;
+?>
+<!-- ******************* The Fastio Links Section ******************* -->
+<section class="fastio-barbie-subpages">
+        <div class="container">
+	<div class="row">
+		<?php while( have_rows('hotwheels-section-3') ): the_row(); 
+			$link_id = get_sub_field('link', false, false);
+		?>
+		<div class="col-<?php echo $links_col; ?>">
+                        <a href="<?php echo get_the_permalink($link_id); ?>">
+			<article style="background-image:url('<?php the_sub_field('img'); ?>');">
+			</article>
+                        </a>
+		</div>
+		<?php endwhile; ?>
+	</div>
+        </div>
+</section><!-- .fastio-img-links -->
+<?php endif; ?>
+
+<?php get_footer(); ?>
