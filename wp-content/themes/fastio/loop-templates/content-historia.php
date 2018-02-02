7f581c74b6fa2a0fce648ebc5a7ea89af0262924
David RS Lopes <davidrslopes@gmail.com>
2018-02-02 00:24:45 +0000

    Bigger Modal

--- wp-content/themes/fastio/loop-templates/content-historia.php
+++ wp-content/themes/fastio/loop-templates/content-historia.php
@@ -27,7 +27,7 @@
 </article><!-- #post-## -->
 <!-- Modal -->
 <div class="modal fade" id="post<?php echo $post_id; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	<div class="modal-dialog">
+	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-body">
 				<div class="row">
