6c806ac239e2c2b442d9db9749d27ee0a1dc98e1
HCarvalho <hugocarvalho@sapo.pt>
2018-02-02 01:16:34 +0000

    Fastio GO - Corrections - Section 1 Hero - Seta , Opçcao de mudar cores , Tamanho do Texto e Sombreado

--- wp-content/themes/fastio/page-templates/fastio-go.php
+++ wp-content/themes/fastio/page-templates/fastio-go.php
@@ -32,12 +32,12 @@ if(!empty($hero_1)):
 	<div class="container-fluid">
 		<div class="row">
 			<header class="col-12">
-				<h1 class="text-center"><?php echo $hero_1['title'];?></h1>
+				<h1 class="text-center" <?php if(!empty($hero_1['cor_do_titulo'])){ echo 'style="color:'.$hero_1['cor_do_titulo'].';"';} ?>><?php echo $hero_1['title'];?></h1>
 			</header>
 		</div>
 	</div>
 	<?php endif; ?>
-	<button class="btn btn-link btn-block fastio-btn-scroll-down"><i class="fa fa-angle-down"></i></button>
+	<button class="btn btn-link btn-block fastio-btn-scroll-down" <?php if(!empty($hero_1['cor_da_seta'])){ echo 'style="color:'.$hero_1['cor_da_seta'].';"';} ?>><i class="fa fa-angle-down"></i></button>
 </section><!-- .fastio-video-hero -->
 <?php endif; ?>
 
