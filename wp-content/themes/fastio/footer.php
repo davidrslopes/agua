01723aa93b7b9aa64a61897bb61a819554527232
David RS Lopes <davidrslopes@gmail.com>
2018-02-09 00:36:14 +0000

    Footer Mobile

--- wp-content/themes/fastio/footer.php
+++ wp-content/themes/fastio/footer.php
@@ -41,19 +41,65 @@ if ( is_front_page() ) : ?>
 		</div><!-- row end -->
 	</footer><!-- footer container end -->
 </div><!-- wrapper end -->
-<?php 
-// ******************* The Main Pages Footer ******************* //
-elseif ( is_page() ) : ?>
+<?php
+// ******************* The Mobile Only Main Pages Footer ******************* //
+elseif(is_page() && wp_is_mobile()): ?>
 <div class="wrapper main-footer-wrapper" id="wrapper-footer-main">
 	<footer class="main-footer <?php echo esc_attr( $container ); ?>" id="colophon">
 		<div class="row">
-			<div class="col-4">
+			<div class="col-2">
+				<!-- Language -->
+				<div class="fastio-footer-lang">
+					<?php do_action('icl_language_selector'); ?>
+				</div>
+			</div>
+			<div class="col-10">
+				<!-- Footer Menu -->
+				<?php wp_nav_menu(
+					array(
+						'theme_location'  => 'footer',
+						'container_class' => 'fastio-footer-nav',
+						'container_id'    => 'fastioFooterNav',
+						'menu_class'      => 'nav pull-right',
+						//'items_wrap'	  => '%3$s',
+						'fallback_cb'     => '',
+						'menu_id'         => 'footer-menu',
+						'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+					)
+				); ?>
+			</div>
+		</div><!-- row end -->
+		<div class="row">
+			<div class="col-8">
 				<!-- Footer Logos -->
 				<div class="fastio-footer-logos">
 					<a href="<?php echo $footer_logos_url;?>"><img src="<?php echo $footer_logos_src;?>" alt="<?php echo $footer_logos_alt;?>" title="<?php echo $footer_logos_title;?>"></a>
 				</div>
 			</div>
-			<div class="col-4 offset-2">
+			<div class="col-4 text-right">
+				<!-- Social -->
+				<ul class="list-inline fastio-footer-social">
+					<li class="list-inline-item"><a href="<?php echo $fb_link; ?>" target="_blank"><i class="fa fa-facebook-square social"></i></a></li>
+					<li class="list-inline-item"><a href="<?php echo $instagr_link; ?>" target="_blank"><i class="fa fa-instagram social"></i></a></li>
+				</ul>
+			</div><!--col end -->
+		</div><!-- row end -->
+	</footer><!-- footer container end -->
+</div><!-- wrapper end -->
+<?php
+else:
+// ******************* The Main Pages Footer ******************* //
+?>
+<div class="wrapper main-footer-wrapper" id="wrapper-footer-main">
+	<footer class="main-footer <?php echo esc_attr( $container ); ?>" id="colophon">
+		<div class="row">
+			<div class="col-12 col-md-4">
+				<!-- Footer Logos -->
+				<div class="fastio-footer-logos">
+					<a href="<?php echo $footer_logos_url;?>"><img src="<?php echo $footer_logos_src;?>" alt="<?php echo $footer_logos_alt;?>" title="<?php echo $footer_logos_title;?>"></a>
+				</div>
+			</div>
+			<div class="col-12 col-md-4 offset-md-2">
 				<!-- Footer Menu -->
 				<?php wp_nav_menu(
 					array(
@@ -68,13 +114,13 @@ elseif ( is_page() ) : ?>
 					)
 				); ?>
 			</div>
-			<div class="col-1 text-right">
+			<div class="col-6 col-md-1 text-right">
 				<!-- Language -->
 				<div class="fastio-footer-lang">
 					<?php do_action('icl_language_selector'); ?>
 				</div>
 			</div>
-			<div class="col-1 text-right">
+			<div class="col-6 col-md-1 text-right">
 				<!-- Social -->
 				<ul class="list-inline fastio-footer-social">
 					<li class="list-inline-item"><a href="<?php echo $fb_link; ?>" target="_blank"><i class="fa fa-facebook-square social"></i></a></li>
@@ -84,45 +130,6 @@ elseif ( is_page() ) : ?>
 		</div><!-- row end -->
 	</footer><!-- footer container end -->
 </div><!-- wrapper end -->
-<?php
-// ******************* The Standard Clean Footer ******************* //
-else: ?>
-<div class="wrapper" id="wrapper-footer">
-
-	<div class="<?php echo esc_attr( $container ); ?>">
-
-		<div class="row">
-
-			<div class="col-12">
-
-				<footer class="site-footer" id="colophon">
-
-					<div class="site-info">
-
-							<a href="<?php  echo esc_url( __( 'http://wordpress.org/','understrap' ) ); ?>"><?php printf( 
-							/* translators:*/
-							esc_html__( 'Proudly powered by %s', 'understrap' ),'WordPress' ); ?></a>
-								<span class="sep"> | </span>
-					
-							<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ), $the_theme->get( 'Name' ),  '<a href="'.esc_url( __('http://understrap.com', 'understrap')).'">understrap.com</a>' ); ?> 
-				
-							(<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Version: %1$s', 'understrap' ), $the_theme->get( 'Version' ) ); ?>)
-					</div><!-- .site-info -->
-
-				</footer><!-- #colophon -->
-
-			</div><!--col end -->
-
-		</div><!-- row end -->
-
-	</div><!-- container end -->
-
-</div><!-- wrapper end -->
-
 <?php endif; ?>
 
 </div><!-- #page we need this extra closing tag here -->
