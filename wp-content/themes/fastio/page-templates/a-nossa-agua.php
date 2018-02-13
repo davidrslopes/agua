05ce995a9890c589540ddccde5019d6814fae195
David RS Lopes <davidrslopes@gmail.com>
2018-02-13 20:49:37 +0000

    ANA; Mobile

--- wp-content/themes/fastio/page-templates/a-nossa-agua.php
+++ wp-content/themes/fastio/page-templates/a-nossa-agua.php
@@ -124,12 +124,14 @@ if(!empty($hero_2)):
 ?>
 <!-- ******************* The Fastio Benefits Section ******************* -->
 <section class="fastio-benefits" data-aos="fade">
-	<img class="ondas ondas1 left top" src="<?php echo get_template_directory_uri()."/img/gotas-esq-1.png" ?>" alt="onda azul l" data-aos="fade-up">
-	<img class="ondas ondas2 left top" src="<?php echo get_template_directory_uri()."/img/gotas-esq-2.png" ?>" alt="onda azul 2" data-aos="fade-up">
-        <img class="ondas ondas3 left top" src="<?php echo get_template_directory_uri()."/img/gotas-esq-3.png" ?>" alt="onda azul 3" data-aos="fade-up">
-        <img class="ondas ondas4 left top" src="<?php echo get_template_directory_uri()."/img/gotas-esq-4.png" ?>" alt="onda azul 4" data-aos="fade-up">
-        <img class="ondas ondas5 left top" src="<?php echo get_template_directory_uri()."/img/gotas-esq-5.png" ?>" alt="onda azul 5" data-aos="fade-up">
-        <div class="container">
+	<div class="decor left">
+		<img class="gotas" src="<?php echo get_template_directory_uri()."/img/gotas-esq-1.png" ?>" alt="onda azul l" data-aos="fade-up">
+		<img class="gotas" src="<?php echo get_template_directory_uri()."/img/gotas-esq-2.png" ?>" alt="onda azul 2" data-aos="fade-up">
+		<img class="gotas" src="<?php echo get_template_directory_uri()."/img/gotas-esq-3.png" ?>" alt="onda azul 3" data-aos="fade-up">
+		<img class="gotas" src="<?php echo get_template_directory_uri()."/img/gotas-esq-4.png" ?>" alt="onda azul 4" data-aos="fade-up">
+		<img class="gotas" src="<?php echo get_template_directory_uri()."/img/gotas-esq-5.png" ?>" alt="onda azul 5" data-aos="fade-up">
+	</div>
+    <div class="container">
 		<div class="row">
 			<?php while( have_rows('a-nossa-agua-section-4') ): the_row(); ?>
 			<div class="col-<?php echo $benefit_col*2; ?> col-md-<?php echo $benefit_col; ?>">
@@ -141,11 +143,12 @@ if(!empty($hero_2)):
 			<?php endwhile; ?>
 		</div>
 	</div>
-	<img class="ondas ondas6 right top" src="<?php echo get_template_directory_uri()."/img/gotas-dir-1.png" ?>" alt="onda azul r1" data-aos="fade-up">
-        <img class="ondas ondas7 right top" src="<?php echo get_template_directory_uri()."/img/gotas-dir-2.png" ?>" alt="onda azul r2" data-aos="fade-up">
-        <img class="ondas ondas8 right top" src="<?php echo get_template_directory_uri()."/img/gotas-dir-3.png" ?>" alt="onda azul r3" data-aos="fade-up">
-        <img class="ondas ondas9 right top" src="<?php echo get_template_directory_uri()."/img/gotas-dir-4.png" ?>" alt="onda azul r4" data-aos="fade-up">
-        
+	<div class="decor right">
+		<img class="gotas" src="<?php echo get_template_directory_uri()."/img/gotas-dir-1.png" ?>" alt="onda azul r1" data-aos="fade-up">
+		<img class="gotas" src="<?php echo get_template_directory_uri()."/img/gotas-dir-2.png" ?>" alt="onda azul r2" data-aos="fade-up">
+		<img class="gotas" src="<?php echo get_template_directory_uri()."/img/gotas-dir-3.png" ?>" alt="onda azul r3" data-aos="fade-up">
+		<img class="gotas" src="<?php echo get_template_directory_uri()."/img/gotas-dir-4.png" ?>" alt="onda azul r4" data-aos="fade-up">
+	</div>
 </section><!-- .fastio-benefits -->
 <?php endif; ?>
 
