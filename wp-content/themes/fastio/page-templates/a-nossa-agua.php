474860baba9eba9ee3d91426355ba93c70994f81
David RS Lopes <davidrslopes@gmail.com>
2018-02-18 12:58:26 +0000

    Mobile: 1 Beneficio por fila

--- wp-content/themes/fastio/page-templates/a-nossa-agua.php
+++ wp-content/themes/fastio/page-templates/a-nossa-agua.php
@@ -145,7 +145,7 @@ if(!empty($hero_2)):
 		<?php endif;?>
 		<div class="row">
 			<?php while( have_rows('a-nossa-agua-section-4') ): the_row(); ?>
-			<div class="col-<?php echo $benefit_col*2; ?> col-md-<?php echo $benefit_col; ?>">
+			<div class="col-12 col-md-<?php echo $benefit_col; ?>">
 				<article data-aos="fade-in">
 					<h5><?php the_sub_field('title'); ?></h5>
 					<p><?php the_sub_field('desc'); ?></p>
