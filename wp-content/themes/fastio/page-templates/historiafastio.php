96ddd702b6d887a483478063864a58138bd38936
Hugo Carvalho <hugo.carvalho@future-healthcare.eu>
2018-02-28 15:06:00 +0000

    Historia Fastio- Correcção para Ordenar Datas

--- wp-content/themes/fastio/page-templates/historiafastio.php
+++ wp-content/themes/fastio/page-templates/historiafastio.php
@@ -242,7 +242,7 @@ if(!empty($fundo_da_pagina)):
 		<div data-aos="fade-up" data-aos-duration="2000">
 			<div class="<?php echo esc_attr( $container ); ?>" id="content">
 				<main class="site-main row" id="main" role="main">
-				<?php $args = array('post_type' => 'historia','posts_per_page' => 4, 'order'=> 'DESC', 'orderby' => 'date' ); ?>
+				<?php $args = array('post_type' => 'historia','posts_per_page' => 4,'meta_key'=> 'ano','orderby'=> 'meta_value', 'order'=> 'ASC' ); ?>
 				<?php $loop = new WP_Query($args); ?>
 				<?php if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
 					<div class="col-12 col-md-3">
@@ -475,7 +475,7 @@ if(!empty($fundo_da_pagina)):
 		<div data-aos="fade-up" data-aos-duration="2000">
 			<div class="<?php echo esc_attr( $container ); ?>" id="content">
 				<main class="site-main row" id="main" role="main">
-				<?php $args = array('post_type' => 'historia','posts_per_page' => 4,'meta_key'=> 'ano','orderby'=> 'meta_value', 'order'=> 'DESC',  ); ?>
+				<?php $args = array('post_type' => 'historia','posts_per_page' => 4,'meta_key'=> 'ano','orderby'=> 'meta_value', 'order'=> 'ASC' ); ?>
 				<?php $loop = new WP_Query($args); ?>
 				<?php if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
 					<div class="col-12 col-md-3 text-center">
