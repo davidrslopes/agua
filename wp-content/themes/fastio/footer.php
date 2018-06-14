7cc5dd57b8022e7ebe4af10f67ae05d8c7721ef5
David RS Lopes <davidrslopes@gmail.com>
2018-06-14 02:19:48 +0100

    Footer Fixes

--- wp-content/themes/fastio/footer.php
+++ wp-content/themes/fastio/footer.php
@@ -47,56 +47,47 @@ if ( is_front_page() ) : ?>
 // ******************* The Mobile Only Main Pages Footer ******************* //
 elseif(is_page() && wp_is_mobile()): ?>
 <div class="wrapper main-footer-wrapper" id="wrapper-footer-main">
-	<footer class="main-footer <?php if(!wp_is_mobile()){echo esc_attr( $container );}else{echo "container-fluid";}?>" id="colophon">
-		<div class="row">
-			<div class="col-11">
-				<!-- Footer Menu -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'footer',
-						'container_class' => 'fastio-footer-nav',
-						'container_id'    => 'fastioFooterNav',
-						'menu_class'      => 'nav pull-left',
-						//'items_wrap'	  => '%3$s',
-						'fallback_cb'     => '',
-						'menu_id'         => 'footer-menu',
-						'walker'          => new understrap_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
-			</div>
-			<div class="col-1">
-				<!-- Language -->
-				<div class="fastio-footer-lang">
-					<?php do_action('icl_language_selector'); ?>
+	<footer class="main-footer" id="colophon">
+		<div class="col">
+			<div class="row">
+				<div class="col-9 col-sm-10">
+					<!-- Footer Menu -->
+					<?php wp_nav_menu(
+						array(
+							'theme_location'  => 'footer',
+							'container_class' => 'fastio-footer-nav',
+							'container_id'    => 'fastioFooterNav',
+							'menu_class'      => 'nav',
+							//'items_wrap'	  => '%3$s',
+							'fallback_cb'     => '',
+							'menu_id'         => 'footer-menu',
+							'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+						)
+					); ?>
 				</div>
-			</div>
-		</div><!-- row end -->
-		<div class="row">
-			<div class="col-8">
-				<!-- Footer Logos -->
-				<div class="fastio-footer-logos">
-					<?php 
-					if(!is_page_template()):
-						if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
-							$lang_code = ICL_LANGUAGE_CODE;
-						}else{
-							$lang_code = 'pt-pt';
-						}
-						echo '<a href="http://savage-agency.pt" target="_blank"><img class="logo-savage" src="'.get_template_directory_uri().'/img/savage-'.$lang_code.'.png" alt="'.__('concebido por SAVAGE', 'understrap').'" title ="'.__('concebido por SAVAGE', 'understrap').'"></a>';
-					else:
-					?>
-					<a href="<?php echo $footer_logos_url;?>"><img class="logo-2020" src="<?php echo $footer_logos_src;?>" alt="<?php echo $footer_logos_alt;?>" title="<?php echo $footer_logos_title;?>"></a>
-					<?php endif; ?>
+				<div class="col-3 col-sm-2">
+					<!-- Language -->
+					<div class="fastio-footer-lang text-right">
+						<?php do_action('icl_language_selector'); ?>
+					</div>
 				</div>
-			</div>
-			<div class="col-4 text-right">
-				<!-- Social -->
-				<ul class="list-inline fastio-footer-social">
-					<li class="list-inline-item"><a href="<?php echo $fb_link; ?>" target="_blank"><i class="fa fa-facebook-square social"></i></a></li>
-					<li class="list-inline-item"><a href="<?php echo $instagr_link; ?>" target="_blank"><i class="fa fa-instagram social"></i></a></li>
-				</ul>
-			</div><!--col end -->
-		</div><!-- row end -->
+			</div><!-- row end -->
+			<div class="row">
+				<div class="col-8">
+					<!-- Footer Logos -->
+					<div class="fastio-footer-logos">
+						<a href="<?php echo $footer_logos_url;?>"><img class="logo-2020" src="<?php echo $footer_logos_src;?>" alt="<?php echo $footer_logos_alt;?>" title="<?php echo $footer_logos_title;?>"></a>
+					</div>
+				</div>
+				<div class="col-4 text-right">
+					<!-- Social -->
+					<ul class="list-inline fastio-footer-social">
+						<li class="list-inline-item"><a href="<?php echo $fb_link; ?>" target="_blank"><i class="fa fa-facebook-square social"></i></a></li>
+						<li class="list-inline-item"><a href="<?php echo $instagr_link; ?>" target="_blank"><i class="fa fa-instagram social"></i></a></li>
+					</ul>
+				</div><!--col end -->
+			</div><!-- row end -->
+		</div><!-- col end -->
 	</footer><!-- footer container end -->
 </div><!-- wrapper end -->
 <?php
@@ -104,26 +95,15 @@ else:
 // ******************* The Main Pages Footer ******************* //
 ?>
 <div class="wrapper main-footer-wrapper" id="wrapper-footer-main">
-	<footer class="main-footer <?php echo esc_attr( $container ); ?>" id="colophon">
+	<footer class="main-footer container" id="colophon">
 		<div class="row">
 			<div class="col-12 col-md-4">
 				<!-- Footer Logos -->
 				<div class="fastio-footer-logos">
-					<?php 
-					if(!is_page_template()):
-						if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
-							$lang_code = ICL_LANGUAGE_CODE;
-						}else{
-							$lang_code = 'pt-pt';
-						}
-						echo '<a href="http://savage-agency.pt" target="_blank"><img class="logo-savage" src="'.get_template_directory_uri().'/img/savage-'.$lang_code.'.png" alt="'.__('concebido por SAVAGE', 'understrap').'" title ="'.__('concebido por SAVAGE', 'understrap').'"></a>';
-					else:
-					?>
 					<a href="<?php echo $footer_logos_url;?>"><img class="logo-2020" src="<?php echo $footer_logos_src;?>" alt="<?php echo $footer_logos_alt;?>" title="<?php echo $footer_logos_title;?>"></a>
-					<?php endif; ?>
 				</div>
 			</div>
-			<div class="col-12 col-md-4 offset-md-2">
+			<div class="col-12 col-md-6 text-right">
 				<!-- Footer Menu -->
 				<?php wp_nav_menu(
 					array(
